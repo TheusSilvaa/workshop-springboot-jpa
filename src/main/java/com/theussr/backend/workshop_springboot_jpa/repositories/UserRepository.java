deca4296ebf21a091c540c536a8322bb64e2d945
Matheus Silva <msilvarodrigues011@gmail.com>
2026-05-19 17:01:30 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/theussr/backend/workshop_springboot_jpa/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.theussr.backend.workshop_springboot_jpa.repositories;
+
+import com.theussr.backend.workshop_springboot_jpa.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

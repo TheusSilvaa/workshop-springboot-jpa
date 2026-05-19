05ae316d75d04bf7a56f171e3686a0c8df5db5ec
Matheus Silva <msilvarodrigues011@gmail.com>
2026-05-19 13:25:12 -0300

    Project created

---
+++ src/test/java/com/theussr/backend/workshop_springboot_jpa/WorkshopSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theussr.backend.workshop_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
